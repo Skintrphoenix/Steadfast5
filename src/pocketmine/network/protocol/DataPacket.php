aaf7320bd4dd869badb8f0457ccc8d9a79b42146
IceCruelStuff <minecraftcool991@gmail.com>
2020-02-22 11:42:36 -0800

    protocol 392

--- src/pocketmine/network/protocol/DataPacket.php
+++ src/pocketmine/network/protocol/DataPacket.php
@@ -95,10 +95,10 @@ abstract class DataPacket extends BinaryStream
     public static function initPackets()
     {
         $oClass = new \ReflectionClass ('pocketmine\network\protocol\Info120');
-        self::$packetsIds[Info::PROTOCOL_120] = $oClass->getConstants();
-        self::$packetsIds[Info::PROTOCOL_200] = $oClass->getConstants();
         self::$packetsIds[Info::PROTOCOL_220] = $oClass->getConstants();
         self::$packetsIds[Info::PROTOCOL_221] = $oClass->getConstants();
+        self::$packetsIds[Info::PROTOCOL_120] = $oClass->getConstants();
+        self::$packetsIds[Info::PROTOCOL_200] = $oClass->getConstants();
         self::$packetsIds[Info::PROTOCOL_240] = $oClass->getConstants();
         self::$packetsIds[Info::PROTOCOL_260] = $oClass->getConstants();
         self::$packetsIds[Info::PROTOCOL_271] = $oClass->getConstants();
@@ -125,6 +125,7 @@ abstract class DataPacket extends BinaryStream
         self::$packetsIds[Info::PROTOCOL_385] = $oClass->getConstants();
         self::$packetsIds[Info::PROTOCOL_386] = $oClass->getConstants();
         self::$packetsIds[Info::PROTOCOL_389] = $oClass->getConstants();
+		self::$packetsIds[Info::PROTOCOL_392] = $oClass->getConstants();
     }
 
 }
