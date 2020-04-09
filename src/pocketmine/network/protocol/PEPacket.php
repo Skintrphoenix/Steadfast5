67a3dff91d697512a052748e2fb50ff36c7a88f3
IceCruelStuff <minecraftcool991@gmail.com>
2020-04-08 23:20:36 -0700

    Merge branch 'master' of https://github.com/Steadfast5/Steadfast5

--- src/pocketmine/network/protocol/PEPacket.php
+++ src/pocketmine/network/protocol/PEPacket.php
@@ -59,6 +59,7 @@ abstract class PEPacket extends DataPacket {
 	
 	public final static function convertProtocol($protocol) {
 		switch ($protocol) {
+			case Info::PROTOCOL_402:
 			case Info::PROTOCOL_401:
 			case Info::PROTOCOL_400:
 				return Info::PROTOCOL_400;
@@ -70,6 +71,7 @@ abstract class PEPacket extends DataPacket {
 			case Info::PROTOCOL_392:
 				return Info::PROTOCOL_392;
 			case Info::PROTOCOL_390:
+				return Info::PROTOCOL_390;
 			case Info::PROTOCOL_389:
 			case Info::PROTOCOL_388:
 				return Info::PROTOCOL_389;
