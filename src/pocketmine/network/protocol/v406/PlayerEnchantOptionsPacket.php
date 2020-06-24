9c4e5fd883fa568f5dcc9cd47073fac20bc9afbf
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-24 10:11:48 +0300

    Update PlayerEnchantOptionsPacket.php

--- src/pocketmine/network/protocol/v406/PlayerEnchantOptionsPacket.php
+++ src/pocketmine/network/protocol/v406/PlayerEnchantOptionsPacket.php
@@ -8,27 +8,27 @@ use pocketmine\network\protocol\Info;
 
 class PlayerEnchantOptionsPacket extends PEPacket {
 
-			const NETWORK_ID = Info::PLAYER_ENCHANT_OPTIONS_PACKET;
-			const PACKET_NAME = "PLAYER_ENCHANT_OPTIONS_PACKET";
+	const NETWORK_ID = Info::PLAYER_ENCHANT_OPTIONS_PACKET;
+	const PACKET_NAME = "PLAYER_ENCHANT_OPTIONS_PACKET";
 
 
-			public $groups;
-			public $items;
+	public $groups;
+	public $items;
 
-		public function decode($playerProtocol) {
+	public function decode($playerProtocol) {
+	}
+
+	public function encode($playerProtocol) {
+		$this->reset($playerProtocol);
+		$this->putVarInt(0); // array size
+
+		foreach ($this->items as $item) {
+			$this->putVarInt(0); // cost
+			$this->putSlot($item);;
+			$this->putString("name"); // enchant name
+			$this->putVarInt(0);  // SimpleServerNetId<struct RecipeNetIdTag, unsinged int, 0>
 		}
 
-		public function encode($playerProtocol) {
-			$this->reset($playerProtocol);
-			$this->putVarInt(0); // array size
-
-			foreach ($this->items as $item) {
-				$this->putVarInt(0); // cost
-				$this->putSlot($item);;
-				$this->putString("name"); // enchant name
-				$this->putVarInt(0);  // SimpleServerNetId<struct RecipeNetIdTag, unsinged int, 0>
-			}
-
-		}
+	}
 
 }
