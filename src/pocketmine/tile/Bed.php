7ce4a2c7e180cdaaa6cfe235f87948b7f46c37fe
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2020-06-27 19:14:45 +0300

    Update Bed.php

--- src/pocketmine/tile/Bed.php
+++ src/pocketmine/tile/Bed.php
@@ -2,12 +2,33 @@
 
 namespace pocketmine\tile;
 
+use pocketmine\level\format\FullChunk;
+use pocketmine\nbt\tag\ByteTag;
 use pocketmine\nbt\tag\Compound;
 use pocketmine\nbt\tag\IntTag;
 use pocketmine\nbt\tag\StringTag;
-use pocketmine\nbt\tag\ByteTag;
 
 class Bed extends Spawnable {
+	
+	/**
+	 * Bed constructor.
+	 *
+	 * @param FullChunk $level
+	 * @param Compound $nbt
+	 */
+	public function __construct(FullChunk $chunk, Compound $nbt){
+		if(!isset($nbt->color) or !($nbt->color instanceof ByteTag)){
+			$nbt->color = new ByteTag("color", 14);
+		}
+		parent::__construct($chunk, $nbt);
+	}
+	
+	/**
+	 * @return int
+	 */
+	public function getColor() : int{
+		return $this->namedtag->color->getValue();
+	}
 
 	public function getSpawnCompound() {
 		return new Compound("", [
@@ -15,7 +36,7 @@ class Bed extends Spawnable {
 			new IntTag("x", (int) $this->x),
 			new IntTag("y", (int) $this->y),
 			new IntTag("z", (int) $this->z),
-			new ByteTag("color", (int) $this->namedtag["color"]),
+			$this->namedtag->color,
 			new ByteTag("isMovable", (int) $this->namedtag["isMovable"])
 		]);
 	}
