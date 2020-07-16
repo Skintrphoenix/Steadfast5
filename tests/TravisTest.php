716393a78543169c2b25ae9af53c0eadc7d25790
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-15 18:00:44 -0700

    Create TravisTest.php

---
+++ tests/TravisTest.php
@@ -0,0 +1,23 @@
+<?php
+
+$server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --disable-readline", [
+	0 => ["pipe", "r"],
+	1 => ["pipe", "w"],
+	2 => ["pipe", "w"]
+], $pipes);
+
+if (!is_resource($server)) {
+	die('Failed to create process');
+}
+
+fwrite($pipes[0], "version\nmakeserver\nstop\n\n");
+fclose($pipes[0]);
+
+echo "\n\nReturn value: ". proc_close($server) ."\n";
+
+if (count(glob("plugins/DevTools/PocketMine-MP*.phar")) === 0) {
+	echo "No server PHAR created!\n";
+	exit(1);
+} else {
+	exit(0);
+}
