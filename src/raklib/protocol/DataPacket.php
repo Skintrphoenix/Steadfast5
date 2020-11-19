cc600c0c4ef4c1737ad7915ff4a5b74ca509bd71
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-19 21:06:44 +0200

    Update DataPacket.php

--- src/raklib/protocol/DataPacket.php
+++ src/raklib/protocol/DataPacket.php
@@ -17,7 +17,7 @@
  * @link http://www.pocketmine.net/
  *
  *
-*/
+ */
 
 namespace raklib\protocol;
 
@@ -25,43 +25,40 @@ namespace raklib\protocol;
 
 abstract class DataPacket extends Packet{
 
-    /** @var EncapsulatedPacket[] */
-    public $packets = [];
+	/** @var EncapsulatedPacket[] */
+	public $packets = [];
+	public $seqNumber;
 
-    public $seqNumber;
+	public function encode(){
+		parent::encode();
+		$this->putLTriad($this->seqNumber);
+		foreach($this->packets as $packet){
+			$this->put($packet instanceof EncapsulatedPacket ? $packet->toBinary() : (string) $packet);
+		}
+	}
 
-    public function encode(){
-        parent::encode();
-        $this->putLTriad($this->seqNumber);
-        foreach($this->packets as $packet){
-            $this->put($packet instanceof EncapsulatedPacket ? $packet->toBinary() : (string) $packet);
-        }
-    }
+	public function length(){
+		$length = 4;
+		foreach($this->packets as $packet){
+			$length += $packet instanceof EncapsulatedPacket ? $packet->getTotalLength() : strlen($packet);
+		}
+		return $length;
+	}
 
-    public function length(){
-        $length = 4;
-        foreach($this->packets as $packet){
-            $length += $packet instanceof EncapsulatedPacket ? $packet->getTotalLength() : strlen($packet);
-        }
+	public function decode(){
+        	parent::decode();
+		$this->seqNumber = $this->getLTriad();
 
-        return $length;
-    }
-
-    public function decode(){
-        parent::decode();
-        $this->seqNumber = $this->getLTriad();
-
-        while(!$this->feof()){
-            $offset = 0;
+		while(!$this->feof()){
 			$data = substr($this->buffer, $this->offset);
-            $packet = EncapsulatedPacket::fromBinary($data, false, $offset);
-            $this->offset += $offset;
-            if(strlen($packet->buffer) === 0){
-                break;
-            }
-            $this->packets[] = $packet;
-        }
-    }
+			if($data == "") {
+				break;
+			}
+			$packet = EncapsulatedPacket::fromBinary($data, false);
+			$this->packets[] = $packet;
+			$this->offset += $packet->getTotalLength();
+		}
+	}
 
 	public function clean(){
 		$this->packets = [];
